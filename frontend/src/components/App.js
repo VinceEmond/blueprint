7370c1af27db6f19e5c888cbe0da16cca9a66a97
Pablo Tack <tackpablo@gmail.com>
2022-04-16 07:48:32 -0700

    Added simple landing page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import NavBar from "./NavBar";
 import { ChakraProvider } from "@chakra-ui/react";
 import Dashboard from "./Dashboard";
+import LandingPage from "./LandingPage";
+import Projects from "./Projects";
+import Tasks from "./Tasks";
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
         <NavBar />
-        <h1>RENDERED HERE</h1>
+        {/* <LandingPage /> */}
         <Dashboard />
+        {/* <Projects /> */}
+        {/* <Tasks /> */}
       </ChakraProvider>
     </div>
   );
