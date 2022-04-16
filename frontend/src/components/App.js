9d15e5e9403710fff523d354a6cba462bb9a9eea
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-15 22:45:30 -0700

    Setup Hashrouter and routes as test

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,30 @@
-import "./App.css";
-import NavBar from "./NavBar";
-import { ChakraProvider } from "@chakra-ui/react";
-import Dashboard from "./Dashboard";
+import './App.css';
+import NavBar from './NavBar';
+import { ChakraProvider, Switch } from '@chakra-ui/react';
+import { HashRouter, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Projects from './Projects';
+import Tasks from './Tasks';
 
 function App() {
   return (
     <div className="App">
-      <ChakraProvider>
-        <NavBar />
-        <h1>RENDERED HERE</h1>
-        <Dashboard />
-      </ChakraProvider>
+      <HashRouter>
+        <ChakraProvider>
+          <NavBar />
+          <h1>RENDERED HERE</h1>
+
+          <div className="content">
+            <Switch>
+              <Route exact path="/Dashboard" component={Dashboard} />
+              <Route path="/Projects" component={Projects} />
+              <Route path="/TAsks" component={Tasks} />
+            </Switch>
+          </div>
+
+          <Dashboard />
+        </ChakraProvider>
+      </HashRouter>
     </div>
   );
 }
