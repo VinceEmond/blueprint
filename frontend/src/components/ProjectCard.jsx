866b27f7bc87675842b17b59d6db8df4afb6ebb6
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-17 16:46:48 -0700

    Start on individual project page (working on link from dash)

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -35,24 +35,26 @@ export default function SocialProfileSimple({ project }) {
           {project.description}
         </Text>
 
-        <Stack mt={8} direction={'row'} spacing={4}>
-          <Button
-            flex={1}
-            fontSize={'sm'}
-            rounded={'full'}
-            bg={'blue.400'}
-            color={'white'}
-            boxShadow={
-              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-            }
-            _hover={{
-              bg: 'blue.500',
-            }}
-            _focus={{
-              bg: 'blue.500',
-            }}>
-            View Project
-          </Button>
+        <Stack mt={8} direction={'row'} spacing={4} justifyContent="center">
+          <Link _hover={{ textDecoration: 'none' }} width="50%" href="/">
+            <Button
+              fontSize={'sm'}
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              alignSelf="center"
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}>
+              View Project
+            </Button>
+          </Link>
         </Stack>
       </Box>
     </Center>
