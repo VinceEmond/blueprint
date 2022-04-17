b9bbaa27842667b799647f1b491a486e3feb4d30
Pablo Tack <tackpablo@gmail.com>
2022-04-16 20:25:44 -0700

    Added all views page for projects and tasks

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -1,5 +1,68 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react'
+import axios from "axios";
+// package that allows conversion of date data
+import moment from "moment";
+
+
 
 export default function Tasks() {
-  return <h1>TASKS PAGE</h1>;
+  const [userTasks, setUserTasks] = useState([]);
+  let loading = false;
+
+  // Retrieve all tasks (eventually user specific tasks)
+  useEffect(() => {
+    if (!loading) {
+      loading = true;
+      axios
+        .get("/api/tasks")
+        .then((response) => {
+          const allTasks = response.data.tasks;
+          setUserTasks(allTasks);
+          console.log(allTasks)
+        })
+        .catch((err) => console.log("err:", err));
+    }
+  }, []);
+
+
+  const taskList = userTasks.map((item) => {
+    // converting date data to more readable data
+    let date = moment(item.due_date).utc().format('YYYY-MM-DD')
+
+    return (
+    <Tr key={item.id}>
+    <Td>{item.name}</Td>
+    <Td>{item.project_id}</Td>
+    <Td>{date}</Td>
+    <Td>{item.status}</Td>
+    <Td>{item.priority}</Td>
+  </Tr>)
+  });
+
+  return <TableContainer>
+  <Table variant='striped' colorScheme='blue'>
+    <Thead>
+      <Tr>
+        <Th>Name</Th>
+        <Th>Project</Th>
+        <Th>Due Date</Th>
+        <Th>Status</Th>
+        <Th>Priority</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+  {taskList}
+    </Tbody>
+
+  </Table>
+</TableContainer>
 }
