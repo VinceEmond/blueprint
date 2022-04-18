4e03f407cdb383e65215426aa25dd0840a1f5112
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-18 00:04:28 -0700

    Removed unnecessary loading flag from projects.jsx

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Table,
   Thead,
@@ -7,43 +7,37 @@ import {
   Th,
   Td,
   TableContainer,
-} from "@chakra-ui/react";
-import axios from "axios";
+} from '@chakra-ui/react';
+import axios from 'axios';
 // package that allows conversion of date data
-import moment from "moment";
+import moment from 'moment';
 
 export default function Projects() {
-  const [userProjects, setUserProjects] = useState([]);
-  let loading = false;
+  const [userProjects, setUserProjects] = useState(null);
 
   // Retrieve all tasks (eventually user specific tasks)
   useEffect(() => {
-    if (!loading) {
-      loading = true;
-      axios
-        .get("/api/projects")
-        .then((response) => {
-          const userProjects = response.data.projects;
-          setUserProjects(userProjects);
-          // console.log(userProjects)
-        })
-        .catch((err) => console.log("err:", err));
-    }
+    axios
+      .get('/api/projects')
+      .then((response) => {
+        const userProjects = response.data.projects;
+        const projectList = userProjects.map((item) => {
+          // converting date data to more readable data
+          let date = moment(item.due_date).utc().format('YYYY-MM-DD');
+
+          return (
+            <Tr key={item.id}>
+              <Td>{item.name}</Td>
+              <Td>{item.owner_id}</Td>
+              <Td>{date}</Td>
+              <Td>{item.status}</Td>
+            </Tr>
+          );
+        });
+        setUserProjects(projectList);
+      })
+      .catch((err) => console.log('err:', err));
   }, []);
-
-  const projectList = userProjects.map((item) => {
-    // converting date data to more readable data
-    let date = moment(item.due_date).utc().format("YYYY-MM-DD");
-
-    return (
-      <Tr key={item.id}>
-        <Td>{item.name}</Td>
-        <Td>{item.owner_id}</Td>
-        <Td>{date}</Td>
-        <Td>{item.status}</Td>
-      </Tr>
-    );
-  });
 
   return (
     <TableContainer>
@@ -56,7 +50,7 @@ export default function Projects() {
             <Th>Status</Th>
           </Tr>
         </Thead>
-        <Tbody>{projectList}</Tbody>
+        <Tbody>{userProjects}</Tbody>
       </Table>
     </TableContainer>
   );
