b412891db05a4e70b8763fe55c73467e0e70eed7
Vince Emond <vince.emond@icloud.com>
2022-04-20 10:45:22 -0700

    Created test schema and seeds for  messages

---
+++ backend/db/seeds/06_messages_seeds.sql
@@ -0,0 +1,9 @@
+-- 06_messages_seeds.sql
+
+INSERT INTO messages (sender_id, content, time_stamp, is_active) VALUES
+  (1, '1st message content of the first message', '2016-06-22 19:10:11', true),
+  (1, '2n message content of the first message', '2017-07-23 3:15:32', true),
+  (1, '3rd message content of the first message', '2018-08-24 4:16:22', true),
+  (1, '4th message content of the first message', '2019-10-23 15:13:22', true),
+  (1, '5th message content of the first message', '2020-11-02 12:15:22', true)
+;
