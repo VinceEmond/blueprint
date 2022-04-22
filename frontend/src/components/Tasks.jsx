e5f1a566e5627ca127c96c1086b19d5797b2f8e6
Pablo Tack <tackpablo@gmail.com>
2022-04-21 23:13:14 -0700

    resolve merge conflicts

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -24,108 +24,102 @@ export default function Tasks() {
   const { currentUser } = useContext(usersContext);
   const [modalState, setModalState] = useState("hide");
   const { isOpen, onOpen, onClose } = useDisclosure();
-<<<<<<< HEAD
-=======
-  const { userProjects, setUserProjects } = useContext(projectsContext);
-  const { userTasks, setUserTasks } = useContext(tasksContext);
->>>>>>> main
 
   // Retrieve all projects (eventually user specific projects)
-  useEffect(() => {
-    axios
-      .get("/api/projects")
-      .then((response) => {
-        const allProjects = response.data.projects;
-        setUserProjects(allProjects);
-      })
-      .catch((err) => console.log("err:", err));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/projects")
+  //     .then((response) => {
+  //       const allProjects = response.data.projects;
+  //       setUserProjects(allProjects);
+  //     })
+  //     .catch((err) => console.log("err:", err));
+  // }, []);
 
-  // Retrieve all tasks (eventually user specific tasks)
-  useEffect(() => {
-    axios
-      .get("/api/tasks")
-      .then((response) => {
-        const allTasks = response.data.tasks;
-        setUserTasks(allTasks);
-        // console.log(allTasks)
-      })
-      .catch((err) => console.log("err:", err));
-  }, [viewValue]);
+  // // Retrieve all tasks (eventually user specific tasks)
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/tasks")
+  //     .then((response) => {
+  //       const allTasks = response.data.tasks;
+  //       setUserTasks(allTasks);
+  //       // console.log(allTasks)
+  //     })
+  //     .catch((err) => console.log("err:", err));
+  // }, [viewValue]);
 
-  const taskList = userTasks.map((item) => {
-    // converting date data to more readable data
-    let date = moment(item.due_date).utc().format("YYYY-MM-DD");
+  // const taskList = userTasks.map((item) => {
+  //   // converting date data to more readable data
+  //   let date = moment(item.due_date).utc().format("YYYY-MM-DD");
 
-    // console.log(`UserProjects: ${userProjects}`);
-    // console.log(`item.project_id: ${item.project_id}`);
+  //   // console.log(`UserProjects: ${userProjects}`);
+  //   // console.log(`item.project_id: ${item.project_id}`);
 
-    let projectName = getProjectName(item.project_id, userProjects);
+  //   let projectName = getProjectName(item.project_id, userProjects);
 
-    function defaultChecks() {
-      let generatedDefaultValue = [];
-      if (item.status === "Complete") {
-        generatedDefaultValue.push(item.name);
-      }
-      return generatedDefaultValue;
-    }
+  //   let generatedDefaultValue = [];
+  //   function defaultChecks() {
+  //     if (item.status === "Complete") {
+  //       generatedDefaultValue.push(item.name);
+  //     }
+  //     return generatedDefaultValue;
+  //   }
+  //   const checkValues = defaultChecks();
 
-    function completeStatusBool() {
-      if (item.status === "Complete") return "grey";
-    }
+  //   function completeStatusBool() {
+  //     if (item.status === "Complete") return "grey";
+  //   }
 
-    const checkValues = defaultChecks();
+  //   function checkClick(e, id) {
+  //     console.log("OLDSTATUS: ", item.status);
+  //     console.log("OLDITEM: ", item);
+  //     console.log("CHECKBOX CLICKED", e.target.checked);
+  //     console.log("CHECKBOX EVENT", e);
+  //     console.log("ITEMID CHECK", id);
+  //     const updatedTasks = updateUserTaskStatus(
+  //       userTasks,
+  //       id,
+  //       e.target.checked
+  //     );
 
-    function checkClick(e, id) {
-      console.log("OLDSTATUS: ", item.status);
-      console.log("OLDITEM: ", item);
-      console.log("CHECKBOX CLICKED", e.target.checked);
-      console.log("CHECKBOX EVENT", e);
-      console.log("ITEMID CHECK", id);
-      const updatedTasks = updateUserTaskStatus(
-        userTasks,
-        id,
-        e.target.checked
-      );
+  //     const filteredTasks = updatedTasks.filter((project) => {
+  //       return project.id === id;
+  //     });
+  //     console.log("FILTEREDPROJECT: ", filteredTasks);
+  //     console.log("NEWSTATUS: ", item.status);
+  //     console.log("NEWITEM: ", item);
 
-      const filteredTasks = updatedTasks.filter((project) => {
-        return project.id === id;
-      });
-      console.log("FILTEREDPROJECT: ", filteredTasks);
-      console.log("NEWSTATUS: ", item.status);
-      console.log("NEWITEM: ", item);
+  //     axios.put(`/api/tasks/${id}`, filteredTasks[0]).then(() => {
+  //       console.log("SUCCESSFUL!");
+  //       setUserTasks(updatedTasks);
+  //     });
+  //   }
 
-      axios.put(`/api/tasks/${id}`, filteredTasks[0]).then(() => {
-        console.log("SUCCESSFUL!");
-        setUserTasks(updatedTasks);
-      });
-    }
-
-    return (
-      // Dylan's temp hack to make sure new task gets unique key
-      <Tr key={item.id || item.description.length * 10} bg={completeStatusBool}>
-        <Td size="sm">
-          <CheckboxGroup defaultValue={checkValues}>
-            <Checkbox
-              ml="2em"
-              value={item.name}
-              onChange={(e) => checkClick(e, item.id)}
-            ></Checkbox>
-          </CheckboxGroup>
-        </Td>
-        <Td>{item.name}</Td>
-        <Td>{projectName}</Td>
-        <Td>{date}</Td>
-        <Td>{item.status}</Td>
-        <Td>{item.priority}</Td>
-      </Tr>
-    );
-  });
+  //   return (
+  //     // Dylan's temp hack to make sure new task gets unique key
+  //     <Tr key={item.id || item.description.length * 10} bg={completeStatusBool}>
+  //       <Td size="sm">
+  //         <CheckboxGroup defaultValue={checkValues}>
+  //           <Checkbox
+  //             ml="2em"
+  //             value={item.name}
+  //             onChange={(e) => checkClick(e, item.id)}
+  //           ></Checkbox>
+  //         </CheckboxGroup>
+  //       </Td>
+  //       <Td>{item.name}</Td>
+  //       <Td>{projectName}</Td>
+  //       <Td>{date}</Td>
+  //       <Td>{item.status}</Td>
+  //       <Td>{item.priority}</Td>
+  //     </Tr>
+  //   );
+  // });
 
   // returns component based on view option
   function View() {
     if (viewValue === "List") {
-      return <TaskTable taskList={taskList} />;
+      return <TaskTable />;
     } else if (viewValue === "Board") {
       return <TrelloTasks />;
     }
