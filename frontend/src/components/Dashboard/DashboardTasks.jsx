80321d4c70d0bd41deb9f951613ee7a1ef3d91a6
Vince Emond <vince.emond@icloud.com>
2022-04-22 22:13:06 -0700

    Checkbox works as expected on dashboard tasks

--- frontend/src/components/Dashboard/DashboardTasks.jsx
+++ frontend/src/components/Dashboard/DashboardTasks.jsx
@@ -14,11 +14,13 @@ import {
   Tab,
   TabPanel,
   Checkbox,
+  CheckboxGroup,
 } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import { tasksContext } from "../../Providers/TasksProvider";
 import { usersContext } from "../../Providers/UsersProvider";
 import { useContext, useEffect } from "react";
+import { updateUserTaskStatus } from "../../helpers/selectors";
 import axios from "axios";
 import moment from "moment";
 
@@ -85,19 +87,56 @@ export default function Tasks({ setModalState, onOpen, onEdit }) {
     );
   };
 
+  function checkClick(e, id) {
+    console.log("id", id);
+    console.log("CHECKBOX CLICKED", e.target.checked);
+
+    const updatedTasks = updateUserTaskStatus(userTasks, id, e.target.checked);
+
+    const filteredTasks = updatedTasks.filter((project) => {
+      return project.id === id;
+    });
+
+    // console.log("FILTEREDPROJECT: ", filteredTasks);
+
+    axios.put(`/api/tasks/${id}`, filteredTasks[0]).then(() => {
+      // console.log("SUCCESSFUL!");
+      setUserTasks(updatedTasks);
+    });
+  }
+
   const tabList = (filter = "all") => {
     const userSpecificTasks = userTasks.filter(
       (task) => task.assignee_id === Number(cookies.id)
     );
     return userSpecificTasks
-      .filter((task) => task.status === filter || filter === "all")
+      .filter(
+        (task) =>
+          task.status === filter ||
+          (filter === "all" && task.status !== "Complete")
+      )
       .map((task) => {
         // For freshly rendered tasks, id will be undefined so make up temp key
         const key = `${filter}+${task.id || task.name.length * 1000}`;
+
+        let generatedDefaultValue = [];
+        function defaultChecks() {
+          if (task.status === "Complete") {
+            generatedDefaultValue.push(task.id);
+          }
+          return generatedDefaultValue;
+        }
+        const checkValues = defaultChecks();
+
         return (
           <Tr key={key}>
             <Td width="5px" padding="2px">
-              <Checkbox></Checkbox>
+              <CheckboxGroup value={checkValues}>
+                <Checkbox
+                  value={task.id}
+                  onChange={(e) => checkClick(e, task.id)}
+                ></Checkbox>
+              </CheckboxGroup>
             </Td>
             <Td onClick={(e) => onEdit(task)} padding="7px">
               {task.name}
