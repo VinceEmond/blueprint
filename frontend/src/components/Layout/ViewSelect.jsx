58fa0cfdd1ac4d76668d6a9e52b14936366dafc3
Pablo Tack <tackpablo@gmail.com>
2022-04-24 14:32:31 -0700

    updated projects table styling

--- frontend/src/components/Layout/ViewSelect.jsx
+++ frontend/src/components/Layout/ViewSelect.jsx
@@ -6,8 +6,23 @@ export default function ViewSelect(props) {
   const { setViewValue, setModalState, onOpen, state } = props;
 
   return (
-    <Flex justify="end" mr="2em">
-      <ButtonGroup variant="outline" spacing="6">
+    <Flex
+      justify="end"
+      style={{
+        backgroundColor: "#0a171e",
+        // opacity: "0.9",
+        color: "white",
+      }}
+    >
+      <ButtonGroup
+        variant="outline"
+        spacing="6"
+        style={{
+          backgroundColor: "#0a171e",
+          // opacity: "0.9",
+          color: "white",
+        }}
+      >
         <IconButton
           aria-label="Search database"
           borderRadius="50%"
