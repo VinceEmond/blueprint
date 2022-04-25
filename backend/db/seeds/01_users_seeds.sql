81c7197ccdb90c7f18df46f90e2c73f452e13a20
Pablo Tack <tackpablo@gmail.com>
2022-04-25 14:26:49 -0700

    Update projects seeds

--- backend/db/seeds/01_users_seeds.sql
+++ backend/db/seeds/01_users_seeds.sql
@@ -1,8 +1,8 @@
 -- 01_users_seeds.sql
 
 INSERT INTO users (first_name, last_name, password, email, avatar, text_alert, is_active) VALUES
-  ('Dylan', 'Pirrotta', '1234', 'test@test.com', 'https://avatars.githubusercontent.com/u/83779291?v=4', false, true),
-  ('Pablo', 'Tack', '1234', 'test1@test.com', 'https://avatars.githubusercontent.com/u/93554557?v=4', false, true),
-  ('Vince', 'Emond', '1234', 'test2@test.com', 'https://i.ibb.co/4WXcywP/Screenshot-of-maya-photo-of-me-35mm.png', false, true),
-  ('Andy', 'Lindsay', '1234', 'test3@test.com', 'https://avatars.githubusercontent.com/u/16171227?v=4', false, true)
+  ('Dylan', 'Pirrotta', '1234', 'test1@test.com', 'https://avatars.githubusercontent.com/u/83779291?v=4', false, true),
+  ('Pablo', 'Tack', '1234', 'test2@test.com', 'https://avatars.githubusercontent.com/u/93554557?v=4', false, true),
+  ('Vince', 'Emond', '1234', 'test3@test.com', 'https://i.ibb.co/4WXcywP/Screenshot-of-maya-photo-of-me-35mm.png', false, true),
+  ('Andy', 'Lindsay', '1234', 'test4@test.com', 'https://avatars.githubusercontent.com/u/16171227?v=4', false, true)
 ;
